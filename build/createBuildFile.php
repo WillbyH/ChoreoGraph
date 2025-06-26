e62d36c2e7344b5f289b251971e2a4880ec27f8f
Willby <willbycraft@gmail.com>
2025-06-27 04:25:36 +1000

    Remove build

--- build/createBuildFile.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $build = file_get_contents('http://'.str_replace("createBuildFile","index",$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']));
-// $file=fopen("output/choreograph.js", "a");
-// fwrite($file, $build);
-// fclose($file);
-file_put_contents("output/choreograph.js", file_get_contents('http://'.str_replace("createBuildFile","index",$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'])));
-
-echo "Build file created in output folder.";
-?>
