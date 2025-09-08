1fdf6d002eb62e03cc8669acf313fcd0561be3ce
Willby <36472703+WillbyH@users.noreply.github.com>
2025-09-08 15:42:51 +1000

    Allow typing dashes into the value track input in the animation editor

--- plugins/animationeditor.js
+++ plugins/animationeditor.js
@@ -1359,7 +1359,7 @@ ChoreoGraph.plugin({
 
           input.oninput = (e) => {
             if (e.target.strictFloatTypeModify) {
-              if (!(["0","1","2","3","4","5","6","7","8","9",".",null].includes(e.data))) {
+              if (!(["0","1","2","3","4","5","6","7","8","9",".","-",null].includes(e.data))) {
                 e.target.value = e.target.previousValue;
                 e.target.blur();
                 return;
